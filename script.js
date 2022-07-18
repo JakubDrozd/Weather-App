8b47e8398205c4b9dd4d20b553b0d27b1417bb2d
Veanty <plmulticraftpl@gmail.com>
2022-07-18 20:33:52 +0200

    Refactor: swap async function for promise

--- script.js
+++
@@ -1,6 +0,0 @@
-let weather = fetch(
-  "http://api.openweathermap.org/geo/1.0/direct?q=Warsaw&limit=5&appid=69d30b66a929001be23d0422d1cff9b8"
-)
-  .then((response) => response.json())
-  .then((response) => {})
-  .catch((err) => console.log(err));
