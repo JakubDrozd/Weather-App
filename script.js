a8ff20e2aa4fdb50ce4fc069d7d113a474ecd852
Veanty <plmulticraftpl@gmail.com>
2022-07-18 19:21:13 +0200

    Feat: Complete weather fetch for current weather

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
-let cityLat = "";
-let cityLon = "";
 let weather = fetch(
   "http://api.openweathermap.org/geo/1.0/direct?q=Warsaw&limit=5&appid=69d30b66a929001be23d0422d1cff9b8"
 )
   .then((response) => response.json())
-  .then((response) => {
-    console.log(response[0].lat);
-  })
+  .then((response) => {})
   .catch((err) => console.log(err));
