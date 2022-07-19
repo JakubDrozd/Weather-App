07efb3790ec09c336d9d6bc8a0e35d2824df0596
Veanty <plmulticraftpl@gmail.com>
2022-07-19 14:47:25 +0200

    Feat: search for country/city weather

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 if (window.navigator.geolocation) {
-  window.navigator.geolocation.getCurrentPosition(
-    successfulLookup,
-    console.log
-  );
+  window.navigator.geolocation.getCurrentPosition(successfulLookup),
+    console.log;
 }
 
 async function successfulLookup(position) {
@@ -25,8 +23,26 @@ async function fetchWeather(place) {
 
 function buildSite(data) {
   console.log(data);
-  const place = document.querySelector(".mb-1.sfw-normal");
+
+  const place = document.querySelector(".sfw-normal");
   place.textContent = `${data.location.name}, ${data.location.country}        ${data.current.observation_time}`;
-  const currentTemp = document.querySelector("p.mb-2:nth-child(1)");
-  currentTemp.innerHTML = `Current temperature: <strong> ${data.current.temperature}°C </strong>`;
+
+  const currentTemp = document.querySelector(".temperature");
+  currentTemp.innerHTML = `<i class="fa-solid fa-temperature-half"></i> Temperature: <strong> ${data.current.temperature}°C </strong>`;
+
+  const currentPressure = document.querySelector(".pressure");
+  currentPressure.innerHTML = `<i class="fa-solid fa-arrow-down"></i> Pressure: <strong> ${data.current.pressure} Pa </strong>`;
+
+  const currentHumidity = document.querySelector(".humidity");
+  currentHumidity.innerHTML = `<i class="fa-solid fa-droplet"></i> Humidity: <strong> ${data.current.humidity}% </strong>`;
+
+  const windSpeed = document.querySelector(".windSpeed");
+  windSpeed.innerHTML = `<i class="fa-solid fa-wind"></i>Wind: <strong> ${data.current.wind_speed}km/h </strong> `;
 }
+
+const searchBtn = document.querySelector("#search-addon");
+searchBtn.addEventListener("click", (event) => {
+  const searchField = document.querySelector(`input[type="search"]`);
+  let city = searchField.value;
+  fetchWeather(city);
+});
